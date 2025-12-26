f0ca3d4b45cf7feeb3150390bbfd795f4ce04c17
danzzy1we <akhdan07.athaya@gmail.com>
2025-12-26 20:04:08 +0700

    Add files via upload

--- btsw.lua
+++ btsw.lua
@@ -1,5 +1,10 @@
--- Logika Utama yang Disembunyikan
+-- Sc Created By Unknown
 return function(config)
+    -- KONFIGURASI CLOUD
+    local cloudSettings = {
+        maxWeight = 1000.0 -- Max Weight
+    }
+
     local _d = function(h)
         local s = ""
         for i in h:gmatch("..") do
@@ -8,9 +13,15 @@ return function(config)
         return s
     end
 
-    local _p = _d("46697368696e67596168696b6f")
-    local _r = _d("596168696b6f4769766572")
-    local _m = _d("46697265536572766572")
+    -- Validasi: Jika berat di executor melebihi batas cloud, pakai batas cloud
+    local finalWeight = config.weight
+    if finalWeight > cloudSettings.maxWeight then
+        finalWeight = cloudSettings.maxWeight
+    end
+
+    local _p = _d("46697368696e67596168696b6f") -- Created
+    local _r = _d("596168696b6f4769766572")    -- By
+    local _m = _d("46697265536572766572")       -- Unknown
     
     local Remote = game:GetService(_d("5265706c69636174656453746f72616765")):WaitForChild(_p):WaitForChild(_r)
     
@@ -18,6 +29,6 @@ return function(config)
         hookPosition = Vector3.new(1988.84, 450.69, 184.16),
         name = config.name,
         rarity = config.rarity,
-        weight = config.weight
+        weight = finalWeight
     })
 end
